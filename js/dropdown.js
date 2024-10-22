618e5c02d2c8267d04efa78dc9ce319c1188d4c6
Adrian290404 <adrianmg2004.29@gmail.com>
2024-10-22 10:35:08 +0200

    images and screen change

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,3 +1,4 @@
+//button and list management
 const openDropdown = document.getElementById('openDropdown')
 const closeDropdown = document.getElementById('closeDropdown')
 const list = document.getElementById('list')
@@ -11,4 +12,21 @@ closeDropdown.addEventListener('click', () => {
     closeDropdown.style.display = 'none'
     openDropdown.style.display = 'block'
     list.style.display = 'none'
+})
+
+//adapt screen change
+const checkChangeScreen = () =>{
+    if (window.matchMedia("(min-width: 1000px)").matches) {
+        closeDropdown.style.display = 'none'
+        openDropdown.style.display = 'none'
+        list.style.display = 'flex'
+    }
+    else{
+        openDropdown.style.display = 'block'
+        closeDropdown.style.display = 'none'
+        list.style.display = 'none'
+    }
+}
+window.addEventListener('resize', () => {
+    checkChangeScreen()
 })
