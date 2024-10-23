2ca2ff7b54c2d39b519e6be5a80049e07c266d82
Adrian290404 <adrianmg2004.29@gmail.com>
2024-10-23 12:30:17 +0200

    popup

---
+++ js/popup.js
@@ -0,0 +1,35 @@
+const popup = document.getElementById('popup')
+const btnClose = document.getElementById('closePopup')
+const emailPattern = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+const popupShowed = () => {
+    return localStorage.getItem('popup') ? true : false
+}
+const showPopup = () => {
+    if (!popupShowed()) {
+        popup.style.display = 'block'
+        localStorage.setItem('popup', true)
+    }
+}
+setTimeout(showPopup, 5000);
+window.addEventListener('scroll', () => {
+    if (!popupShowed()) {
+        const scrollHeight = document.body.scrollHeight
+        const scrollTop = window.scrollY
+        const clientHeight = window.innerHeight
+        const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100
+        if (scrollPercentage > 25){
+            showPopup()
+        }
+    }
+});
+btnClose.addEventListener('click', () => {popup.style.display = 'none'})
+window.addEventListener('click', (e) => {
+    if (popupShowed() && popup.style.display === 'block' && !popup.contains(e.target)) { 
+        popup.style.display = 'none'
+    }
+})
+window.addEventListener('keyup', (e) => {
+    if (popupShowed() && popup.style.display === 'block' && e.key === 'Escape') {
+        popup.style.display = 'none'
+    }
+})
