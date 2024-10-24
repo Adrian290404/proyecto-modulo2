3ed812627e2889469be6b887d729b7cc6ee834fe
Adrian290404 <adrianmg2004.29@gmail.com>
2024-10-24 14:19:29 +0200

    Testing the API on both forms and validating the form in the popup

--- js/form.js
+++ js/form.js
@@ -3,12 +3,15 @@
 const namePattern = new RegExp('^(?=.*[a-zA-Z]{2})[a-zA-Z\\s]{2,100}$')
 const emailPattern = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 const form = document.getElementById('form')
+const formSubscribe = document.getElementById('subscribe')
+const formSubscribeEmail = document.getElementById('emailSubscribe')
 const formName = document.getElementById("name")
 const formEmail = document.getElementById("email")
 const checkBox = document.getElementById("checkbox")
 const textName = document.getElementById("textAlert")
 const textEmail = document.getElementById("emailAlert")
 const textCheckbox = document.getElementById("checkboxAlert")
+const url = 'https://jsonplaceholder.typicode.com/posts'
 let checkBoxChecked = false
 
 formName.addEventListener("change", () => {
@@ -20,6 +23,30 @@ formEmail.addEventListener("change", () => {
 checkBox.addEventListener("change", () => {
     checkBox.checked ? (checkBoxChecked = true, textCheckbox.classList.add('none')) : checkBoxChecked = false
 })
+const api = (urlApi, jsonApi) => {
+    fetch(urlApi, {
+        method: 'POST',
+        body: JSON.stringify(jsonApi),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+        })
+        .then((response) => {
+            if (response.ok){
+                return response.json().then((json) => {
+                    console.log('Form data: ', json)       
+                    form.reset()
+                    alert("Form submitted successfully!")
+                })
+            }
+            else{
+                throw new Error("Error on network response")
+            }
+        })
+        .catch((error) => {
+            console.log('Error: '+error)
+        })
+}
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     if (!namePattern.test(formName.value)) {
@@ -37,33 +64,32 @@ form.addEventListener("submit", (e) => {
     if (namePattern.test(formName.value) && emailPattern.test(formEmail.value) && checkBoxChecked) {
         // API Testing
 
-        const url = 'https://jsonplaceholder.typicode.com/posts'
         const formJson = {
             title: 'form',
             name: formName.value,
             email: formEmail.value,
             userId: 1,
         }
-        fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(formJson),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-        })
-        .then((response) => {
-            if (response.ok){
-                return response.json().then((json) => console.log('Form data: ', json))
-            }
-            else{
-                throw new Error("Error on network response")
-            }
-        })
-        .catch((error) => {
-            throw new Error('Error: '+error)
-        })
-
-        form.reset()
-        alert("Form submitted successfully!")
+        api(url, formJson)
     }
+})
+
+// Popup validation
+formSubscribe.addEventListener("submit", (e) => {
+    e.preventDefault()
+    if (!emailPattern.test(formSubscribeEmail.value)) {
+        formSubscribeEmail.classList.add('red')
+    }
+    else{
+        formSubscribeEmail.classList.remove('red')
+    }
+    if (emailPattern.test(formSubscribeEmail.value)){
+        const formJson = {
+            title: 'form subscribe',
+            email: formSubscribeEmail.value,
+            userId: 2,
+        }
+        api(url, formJson)
+    }
+
 })
